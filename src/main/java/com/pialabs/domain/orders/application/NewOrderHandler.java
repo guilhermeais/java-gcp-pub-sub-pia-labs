3a5b86243a10706c5fa3b6290bdaf126864c7bf4
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-04-03 21:19:13 -0300

    feat(NewOrderHandler): always ack

--- src/main/java/com/pialabs/domain/orders/application/NewOrderHandler.java
+++ src/main/java/com/pialabs/domain/orders/application/NewOrderHandler.java
@@ -1,10 +1,5 @@
 package com.pialabs.domain.orders.application;
 
-import java.lang.reflect.Type;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import com.google.cloud.pubsub.v1.MessageReceiver;
 import com.google.cloud.pubsub.v1.Subscriber;
 import com.google.gson.FieldNamingPolicy;
@@ -18,105 +13,131 @@ import com.google.gson.JsonSyntaxException;
 import com.google.pubsub.v1.ProjectSubscriptionName;
 import com.pialabs.domain.orders.core.models.Order;
 import com.pialabs.domain.orders.core.ports.incoming.SaveOrder;
+import java.lang.reflect.Type;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class NewOrderHandler {
-    private boolean isJUnitTest = System.getProperty("org.junit.jupiter") != null;
 
-    private final String projectId;
-    private final String subscriptionId;
-    private final SaveOrder saveOrder;
+  private boolean isJUnitTest = System.getProperty("org.junit.jupiter") != null;
 
-    private Subscriber subscriber;
+  private final String projectId;
+  private final String subscriptionId;
+  private final SaveOrder saveOrder;
 
-    public NewOrderHandler(
-            SaveOrder saveOrder) {
-        this.projectId = System.getenv("PROJECT_ID");
-        this.subscriptionId = System.getenv("SUBSCRIPTION_ID");
+  private Subscriber subscriber;
 
-        if (projectId == null || projectId.isEmpty()) {
-            throw new IllegalArgumentException("PROJECT_ID environment variable must be set.");
-        }
-
-        if (subscriptionId == null || subscriptionId.isEmpty()) {
-            throw new IllegalArgumentException("SUBSCRIPTION_ID environment variable must be set.");
-        }
-
-        this.saveOrder = saveOrder;
-    }
-
-    public void start() {
-        ProjectSubscriptionName subscriptionName = ProjectSubscriptionName.of(projectId, subscriptionId);
-        MessageReceiver receiver = createMessageReceiver();
-
+  private final Gson gson = new GsonBuilder()
+    .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+    .registerTypeAdapter(
+      Date.class,
+      (JsonDeserializer<Date>) (json, typeOfT, context) -> {
         try {
-            subscriber = startSubscriber(subscriptionName, receiver);
-            waitForMessages(subscriber, subscriptionName);
-        } catch (Exception e) {
-            System.out.println("Error occurred while subscribing: " + e.getMessage());
+          return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+            .parse(json.getAsString());
+        } catch (ParseException e) {
+          throw new JsonParseException(e);
         }
+      }
+    )
+    .create();
+
+  public NewOrderHandler(SaveOrder saveOrder) {
+    this.projectId = System.getenv("PROJECT_ID");
+    this.subscriptionId = System.getenv("SUBSCRIPTION_ID");
+
+    if (projectId == null || projectId.isEmpty()) {
+      throw new IllegalArgumentException(
+        "PROJECT_ID environment variable must be set."
+      );
     }
 
-    public void stop() {
-        if (subscriber != null) {
-            subscriber.stopAsync().awaitTerminated();
-
-            System.out.println("Subscriber stopped.");
-
-            subscriber = null;
-        }
+    if (subscriptionId == null || subscriptionId.isEmpty()) {
+      throw new IllegalArgumentException(
+        "SUBSCRIPTION_ID environment variable must be set."
+      );
     }
 
-    private MessageReceiver createMessageReceiver() {
-        return (message, consumer) -> {
-            System.out.println("Id: " + message.getMessageId());
-            System.out.println("Data: " + message.getData().toStringUtf8());
+    this.saveOrder = saveOrder;
+  }
 
-            String json = message.getData().toStringUtf8();
-            Gson gson = new GsonBuilder()
-                    .registerTypeAdapter(Date.class, new JsonDeserializer<Date>() {
-                        @Override
-                        public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
-                                throws JsonParseException {
-                            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                            try {
-                                return formatter.parse(json.getAsString());
-                            } catch (ParseException e) {
-                                throw new JsonParseException(e);
-                            }
-                        }
-                    })
-                    .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-                    .create();
+  public void start() {
+    ProjectSubscriptionName subscriptionName = ProjectSubscriptionName.of(
+      projectId,
+      subscriptionId
+    );
+    MessageReceiver receiver = createMessageReceiver();
 
-            try {
-                Order order = gson.fromJson(json, Order.class);
-                saveOrder.handle(order);
-
-                consumer.ack();
-            } catch (JsonSyntaxException e) {
-                System.out.println("Error occurred while parsing JSON: " + e.getMessage());
-            } catch (Exception e) {
-                System.out.println("Error occurred while saving order: " + e.getMessage());
-                e.printStackTrace();
-                consumer.nack();
-            }
-        };
+    try {
+      subscriber = startSubscriber(subscriptionName, receiver);
+      waitForMessages(subscriber, subscriptionName);
+    } catch (Exception e) {
+      System.out.println("Error occurred while subscribing: " + e.getMessage());
     }
+  }
 
-    private Subscriber startSubscriber(ProjectSubscriptionName subscriptionName, MessageReceiver receiver) {
-        System.out.println("Starting subscriber...");
-        System.out.println("IsTesting: " + isJUnitTest);
+  public void stop() {
+    if (subscriber != null) {
+      subscriber.stopAsync().awaitTerminated();
 
-        Subscriber subscriber = Subscriber.newBuilder(subscriptionName, receiver).build();
+      System.out.println("Subscriber stopped.");
 
-        subscriber.startAsync().awaitRunning();
-        return subscriber;
+      subscriber = null;
     }
+  }
 
-    private void waitForMessages(Subscriber subscriber, ProjectSubscriptionName subscriptionName) {
-        System.out.printf("Listening for messages on %s:\n", subscriptionName.toString());
-        while (subscriber.isRunning()) {
-        }
-    }
+  private MessageReceiver createMessageReceiver() {
+    return (message, consumer) -> {
+      System.out.println("Id: " + message.getMessageId());
+      System.out.println("Data: " + message.getData().toStringUtf8());
 
+      String json = message.getData().toStringUtf8();
+      System.out.println("Json: " + json);
+
+      try {
+        Order order = gson.fromJson(json, Order.class);
+        saveOrder.handle(order);
+      } catch (JsonSyntaxException e) {
+        System.out.println(
+          "Error occurred while parsing JSON: " + e.getMessage()
+        );
+
+        e.printStackTrace();
+      } catch (Exception e) {
+        System.out.println(
+          "Error occurred while saving order: " + e.getMessage()
+        );
+        e.printStackTrace();
+      } finally {
+        consumer.ack();
+      }
+    };
+  }
+
+  private Subscriber startSubscriber(
+    ProjectSubscriptionName subscriptionName,
+    MessageReceiver receiver
+  ) {
+    System.out.println("Starting subscriber...");
+    System.out.println("IsTesting: " + isJUnitTest);
+
+    Subscriber subscriber = Subscriber
+      .newBuilder(subscriptionName, receiver)
+      .build();
+
+    subscriber.startAsync().awaitRunning();
+    return subscriber;
+  }
+
+  private void waitForMessages(
+    Subscriber subscriber,
+    ProjectSubscriptionName subscriptionName
+  ) {
+    System.out.printf(
+      "Listening for messages on %s:\n",
+      subscriptionName.toString()
+    );
+    while (subscriber.isRunning()) {}
+  }
 }
